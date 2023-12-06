ef2ea759f81f5457500bb09ce4a22bd9152dcd6a
suwandre <suwandresukijat@gmail.com>
2023-12-06 18:10:31 +0100

    chore: updated package.json; updated next.config.js with security settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,43 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withFonts = require('next-fonts');
+const withVideos = require('next-videos');
+const { createSecureHeaders } = require('next-secure-headers');
 
-module.exports = nextConfig
+module.exports = withVideos(
+  withFonts({
+    webpack(config, options) {
+      return config;
+    },
+    reactStrictMode: false,
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: createSecureHeaders({
+            contentSecurityPolicy: {
+              directives: {
+                frameAncestors: [
+                  'self',
+                  'vitals.vercel-insights.com',
+                  'https://assets.vercel.com',
+                ],
+                styleSrc: ["'self'", "'unsafe-inline'"],
+                imgSrc: [
+                  "'self'",
+                ],
+                baseUri: 'self',
+              },
+            },
+            forceHTTPSRedirect: [
+              true,
+              { maxAge: 63072000, includeSubDomains: true },
+            ],
+            noopen: 'noopen',
+            nosniff: 'nosniff',
+            xssProtection: 'sanitize',
+            referrerPolicy: 'strict-origin-when-cross-origin',
+          }),
+        },
+      ];
+    },
+  })
+);
