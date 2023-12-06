4d7fcdb74c35bd54a227744757bb0e3a495b5eb4
suwandre <suwandresukijat@gmail.com>
2023-12-06 18:15:54 +0100

    chore: initialized mantineprovider; added base global styles

---
+++ src/components/Globals/colors.ts
@@ -0,0 +1,5 @@
+export const COLORS = {
+    green: '#42ca9f',
+    red: '#ca4242',
+  };
+  
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core';
+import { COLORS } from '@/components/Globals/colors';
+import GlobalStyles from '@/components/Globals/Styles';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: 'dark',
+        fontFamily: 'Chakra Petch, sans-serif',
+        headings: { fontFamily: 'Chakra Petch, sans-serif' },
+        loader: 'oval',
+        colors: {
+          nbcGreen: [COLORS.green],
+          nbcRed: [COLORS.red],
+        },
+      }}
+    >
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </MantineProvider>
+  )
 }
