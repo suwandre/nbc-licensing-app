9a645e36825c931ca441e59665c5c11589c72a18
suwandre <suwandresukijat@gmail.com>
2024-01-01 22:04:38 +0000

    chore: updated domain for provider auth

--- src/pages/api/moralis/[...moralis].ts
+++ src/pages/api/moralis/[...moralis].ts
@@ -3,7 +3,7 @@ import { MoralisNextApi } from '@moralisweb3/next';
 export default MoralisNextApi({
   apiKey: process.env.MORALIS_API_KEY ?? '',
   authentication: {
-    domain: 'webapp.nbcompany.io',
+    domain: 'nbc-licensing-app.vercel.app',
     uri: process.env.NEXTAUTH_URL ?? '',
     timeout: 120,
   },
